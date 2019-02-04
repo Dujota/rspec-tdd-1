efde9751b1c738c40a876593898821d65a869332
Denis <ddujota@internationalsafety.com>
2019-02-04 11:04:52 -0500

    first spec writted

--- tests/hello/hello_spec.rb
+++ tests/hello/hello_spec.rb
@@ -1,6 +1,9 @@
 require_relative "hello"
 
 RSpec.describe Hello do
-  context "" do
+  it "says 'hello world" do
+    greeting = Hello.say_hello
+    #expect output from this line to match "Hello World!"
+    expect(greeting).to eq("Hello Workd!")
   end
 end
---
+++ tests/hello/runner.rb
@@ -0,0 +1,3 @@
+require_relative "hello"
+
+# we will run the hello program to see how it works aftert it is implemented in rspec
