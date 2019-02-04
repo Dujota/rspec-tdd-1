548523995792819bfd3e9173a40bc5838ca2c109
Denis <ddujota@internationalsafety.com>
2019-02-04 12:29:36 -0500

    setup the scaffold for the palindrome specs

--- tests/palindrome/palindrome_spec.rb
+++ tests/palindrome/palindrome_spec.rb
@@ -1,4 +1,13 @@
 require_relative "palindrome"
 
-RSpec.describe "" do
+RSpec.describe Word do
+  context "test word is a palindrome" do
+    it "should read the same forwards as backwords" do
+    end
+  end
+
+  context "test word is not a palindrome" do
+    it "should NOT read the same forwards as backwords" do
+    end
+  end
 end
