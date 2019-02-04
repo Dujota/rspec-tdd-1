400b3e1fc198d100da4dc5f3dd9d6885df188635
Denis <ddujota@internationalsafety.com>
2019-02-04 11:11:45 -0500

    passing test spec

--- tests/hello/hello_spec.rb
+++ tests/hello/hello_spec.rb
@@ -1,9 +1,9 @@
 require_relative "hello"
 
 RSpec.describe Hello do
-  it "says 'hello world" do
+  it "say_hello 'Hello world!" do
     greeting = Hello.say_hello
     #expect output from this line to match "Hello World!"
-    expect(greeting).to eq("Hello Workd!")
+    expect(greeting).to eq("Hello World!")
   end
 end
--- tests/hello/runner.rb
+++ tests/hello/runner.rb
@@ -1,3 +1,4 @@
 require_relative "hello"
 
 # we will run the hello program to see how it works aftert it is implemented in rspec
+puts Hello.say_hello
