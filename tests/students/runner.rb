f675d5f591fd3222dd043b8f36e27fe5cb74f273
Denis <ddujota@internationalsafety.com>
2021-06-02 13:01:18 -0500

    runner updated

--- tests/students/runner.rb
+++ tests/students/runner.rb
@@ -1,5 +1,8 @@
 require_relative "student"
 
-test_student = Student.new("Test", "Student")
+test1_student = Student.new("Test", "Student")
+test2_student = Student.new("Test2", "Student")
+test3_student = Student.new("Test3", "Student")
 
-puts test_student.first_name
+puts test1_student.first_name
+puts Student.total_count
