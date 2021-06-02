db378ae2c86c15bb5776765347713678d386b676
Denis <ddujota@internationalsafety.com>
2021-06-02 13:25:14 -0500

    added runner file

---
+++ tests/calculator/runner.rb
@@ -0,0 +1,9 @@
+require_relative "calculator"
+num1 = 10
+num2 = 50
+puts Calculator.exponent 8, 3
+
+puts Calculator.divide num1, num2
+puts Calculator.multiply num1, num2
+puts Calculator.subtract num1, num2
+puts Calculator.add num1, num2
